c98a5dac64381c4a4ef9e5b4175366ab198b4d42
Rizlrad Fz <rizlradfz@gmail.com>
2025-08-21 17:12:06 +0700

    Refactor workflow and configuration for Next.js site deployment
    Testing Workflows

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: '/webprofilebaim',
+    assetPrefix: '/webprofilebaim/',
+};
 
 export default nextConfig;
