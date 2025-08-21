aecd2a9f0eb75208b7e2d42d8a533b41d3f957d7
Rizlrad Fz <rizlradfz@gmail.com>
2025-08-21 17:48:49 +0700

    refactor: comment out output option in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
     // enable the static HTML exporter (next build will produce out/)
     output: 'export',
-    basePath: '/webprofilebaim',
-    assetPrefix: '/webprofilebaim/',
+    // basePath: '/webprofilebaim',
+    // assetPrefix: '/webprofilebaim/'
 };
 
 export default nextConfig;
